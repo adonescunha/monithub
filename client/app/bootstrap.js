e7c4858fdb8b73269d37cbf3bf2a2f7209b11005
Adones Cunha <adonescunha@gmail.com>
2016-01-13 12:57:27 -0300

    jspm configuration

--- client/app/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import mainModule from './app/main.js';
-
-angular.element(document).ready(function() {
-  angular.bootstrap(document, [mainModule.name], { strictDi: true });
-});
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,10 @@
 var gulp = require('gulp'),
     useref = require('gulp-useref');
 
-var src = './layout/monithub/_site/css/*.css';
-
 gulp.task('copy', function() {
+  var src = './layout/monithub/_site/css/*.css';
   gulp.src(src)
-    .pipe(gulp.dest('./client/css'));
-
-  gulp.src('client/app/**/*')
-    .pipe(gulp.dest('./public/app'));
-});
-
-gulp.task('bundle', function() {
-  gulp.src('client/*.html')
-    .pipe(useref())
-    .pipe(gulp.dest('public'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch('client/**/*', ['copy', 'bundle'])
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('express', function() {
@@ -27,4 +13,4 @@ gulp.task('express', function() {
 
 gulp.task('serve', ['express']);
 
-gulp.task('default', ['serve', 'watch'])
+gulp.task('default', ['serve'])
