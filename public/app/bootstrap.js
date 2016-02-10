8a0e6e038098a83bbafb98f280bcce59d29cc909
Adones Cunha <adonescunha@gmail.com>
2016-02-10 15:49:03 -0300

    Ensure bootstrap css is loaded first

--- public/app/bootstrap.js
+++ public/app/bootstrap.js
@@ -1,10 +1,3 @@
-import 'bootstrap/css/bootstrap.css!';
-import 'almasaeed2010/AdminLTE/dist/css/AdminLTE.css!';
-import 'almasaeed2010/AdminLTE/dist/css/skins/skin-blue.css!';
-import 'font-awesome/css/font-awesome.css!';
-import 'eu81273/angular.snackbar/css/angular.snackbar.css!';
-import '/css/main.css!';
-
 import app from './main';
 
 angular.element(document).ready(function() {
---
+++ public/app/styles.js
@@ -0,0 +1,5 @@
+import 'almasaeed2010/AdminLTE/dist/css/AdminLTE.css!';
+import 'almasaeed2010/AdminLTE/dist/css/skins/skin-blue.css!';
+import 'font-awesome/css/font-awesome.css!';
+import 'eu81273/angular.snackbar/css/angular.snackbar.css!';
+import '/css/main.css!';
